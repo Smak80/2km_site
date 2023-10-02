9b75251505bbb07e8685d1f4d6860bc377cd3c25
Sergey Makletsov <smaklets@kpfu.ru>
2023-10-02 20:19:46 +0300

    примеры функций для работы со строками

--- index.php
+++ index.php
@@ -4,7 +4,11 @@ require_once ("common/a_content.php");
 
 class index extends \common\a_content {
     public function show_content(): void{
-        print "Контент главной страницы";
+        $text = "один+два=три";
+        $filtered_text = mb_ereg_replace("[^a-zа-я]", "", $text);
+        print($filtered_text);
+        $arr = array_unique(mb_str_split($filtered_text));
+        print_r($arr);
     }
 }
 
