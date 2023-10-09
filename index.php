1366fe481b55341880ab0ea3514bbd6946a2f025
Sergey Makletsov <smaklets@kpfu.ru>
2023-10-09 18:32:34 +0300

    обновления от 2023-10-09

--- index.php
+++ index.php
@@ -5,9 +5,19 @@ require_once ("common/a_content.php");
 class index extends \common\a_content {
     public function show_content(): void{
         $text = "один+два=три";
+        $keys = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         $filtered_text = mb_ereg_replace("[^a-zа-я]", "", $text);
         print($filtered_text);
-        $arr = array_unique(mb_str_split($filtered_text));
+        $arr = array_combine(
+            $keys,
+            array_pad(
+                array_unique( mb_str_split($filtered_text) ),
+                10,
+                ''
+            )
+        );
+        $res = eval("return 0123+245==368;");
+        print("<br>Результат: $res<br>");
         print_r($arr);
     }
 }
